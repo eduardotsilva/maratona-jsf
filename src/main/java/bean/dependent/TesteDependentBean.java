6ad282dce2dfe4affe59763d597e23d132e201ab
NoteEdu <eduardo.tsilva@hotmail.com>
2021-02-13 21:09:36 -0300

    JSF Aula 21 - Escopo Dependent

---
+++ src/main/java/bean/dependent/TesteDependentBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean.dependent;
+
+import java.io.Serializable;
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Edu
+ */
+@Named
+@Dependent
+public class TesteDependentBean implements Serializable {
+    
+}
